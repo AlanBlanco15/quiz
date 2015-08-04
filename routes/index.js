9b0201c8aaff3b93297f26fc2c3eeb8e0e0731c4
AlanBlanco15 <alanblanco15@gmail.com>
2015-08-03 19:57:06 -0500

    listadepreguntas

--- routes/index.js
+++ routes/index.js
@@ -9,8 +9,16 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+//autoload
+
+router.param('quizId', quizController.load);
+
+//definicion de rutas de /quizes
+router.get('/quizes',  quizController.index);
+router.get('/quizes/:quizId(\\d+)', quizController.show);
+
 router.get('/quizes/question' , quizController.question);
-router.get('/quizes/answer', quizController.answer);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 
 
 
